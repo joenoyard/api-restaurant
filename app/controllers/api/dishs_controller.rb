b4ae0c39dd92adad2edbea3e1d7f13541cb2ad37
Supphachai Chobjit <joe@gettgo.com>
2019-05-13 14:25:42 +0700

    remove pretty

--- app/controllers/api/dishs_controller.rb
+++ app/controllers/api/dishs_controller.rb
@@ -1,16 +1,16 @@
 class Api::DishsController < ApplicationController
   def index
     @dish = Dish.all.as_json(only: %i[id name])
-    render json: JSON.pretty_generate(dishs: @dish)
+    render json: { dishs: @dish }
   end
 
   def show
     @dish = Dish.where(id: params[:id]).as_json(only: %i[id name])
-    render json: JSON.pretty_generate(dishs: @dish)
+    render json: { dishs: @dish }
   end
 
   def get_restaurants
     @restaurants = Dish.where(id: params[:id]).first.restaurants.as_json(only: %i[id name])
-    render json: JSON.pretty_generate(restaurants: @restaurants)
+    render json: { restaurants: @restaurants }
   end
 end
--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,16 +1,16 @@
 class Api::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all.as_json(only: %i[id name])
-    render json: JSON.pretty_generate(restaurants: @restaurants)
+    render json: { restaurants: @restaurants }
   end
 
   def show
     @restaurants = Restaurant.where(id: params[:id]).as_json(only: %i[id name])
-    render json: JSON.pretty_generate(restaurants: @restaurants)
+    render json: { restaurants: @restaurants }
   end
 
   def get_dishs
     @dish = Restaurant.where(id: params[:id]).first.dishs.as_json(only: %i[id name])
-    render json: JSON.pretty_generate(dishs: @dish)
+    render json: { dishs: @dish }
   end
 end
